eefb3ce856ca4d9182d8feb37c6a137e0d361ae2
moonRider <solar.pom@gmail.com>
2019-12-10 21:34:21 +0800

    run webpack server

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path'),
+      webpack = require('webpack'),
+      { CleanWebpackPlugin } = require('clean-webpack-plugin'),
+      HtmlWebpackPlugin = require('html-webpack-plugin'),
+      FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
+  entry: {
+    app: './src/app.js',
+  },
+  output: {
+    filename: '[name]-[hash].js',
+    path: path.resolve(process.cwd(), './dist'),
+    publicPath: '/',
+  },
+  devServer: {
+    clientLogLevel: 'warning',
+    contentBase: [path.resolve(process.cwd(), './dist')],
+    disableHostCheck: true,
+    port: 8000,
+    host: '0.0.0.0',
+    compress: true,
+    hot: true,
+    historyApiFallback: {},
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'indoor-map',
+      filename: 'index.html',
+      template: './src/index.html',
+      meta: {
+        viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+      },
+    }),
+    new FriendlyErrorsPlugin(),
+  ]
+}
