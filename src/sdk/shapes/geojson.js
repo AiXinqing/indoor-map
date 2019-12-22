63da7041ca6c614183d5640bb88cd3390714eb57
moonRider <solar.pom@gmail.com>
2019-12-22 23:38:20 +0800

    add geojson shape base

--- src/sdk/shapes/geojson.js
+++ src/sdk/shapes/geojson.js
@@ -1,6 +1,16 @@
 import IndoorShape from './shape'
 import { SvgNs } from '../constant'
 
+const convertGeojsonToPath = (geojson) => {
+  return ''
+}
+
 export default class IndoorGeoJson extends IndoorShape {
-  constructor (geojson) {}
+  constructor (geojson) {
+    super()
+    const path = document.createElementNS(SvgNs, 'path')
+    const dstr = convertGeojsonToPath(geojson)
+    path.setAttribute('d', dstr)
+    this.$el = path
+  }
 }
