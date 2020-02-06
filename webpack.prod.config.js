4cdd7146d6d82db2c33fd4c7f16d1c897ef8f197
moonRider <solar.pom@gmail.com>
2020-02-07 01:42:26 +0800

    add build script

---
+++ webpack.prod.config.js
@@ -0,0 +1,45 @@
+const path = require('path'),
+      { CleanWebpackPlugin } = require('clean-webpack-plugin'),
+      HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  devtool: 'cheap-module-eval-source-map',
+  entry: {
+    app: './src/app.js',
+  },
+  output: {
+    filename: '[name]-[hash].js',
+    path: path.resolve(process.cwd(), './dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          { loader: 'css-loader', options: { importLoaders: 1, sourceMap: true } },
+        ]
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'indoor-map',
+      filename: 'index.html',
+      template: './src/index.html',
+      meta: {
+        viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      assets: path.resolve(process.cwd(), './src/assets'),
+    },
+  }
+}
