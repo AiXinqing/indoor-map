15e2906faf15cb619eb8dcb8ef5a7f0d36a82adb
moonRider <solar.pom@gmail.com>
2020-02-11 21:58:52 +0800

    add circle shape

---
+++ src/sdk/shapes/circle.js
@@ -0,0 +1,27 @@
+import IndoorShape from "./shape"
+import { SvgNs } from "../constant"
+
+export default class IndoorCircle extends IndoorShape {
+  constructor () {
+    super(...arguments)
+  }
+
+  initialShape (shape, styles = {}) {
+    const circle = document.createElementNS(SvgNs, 'path')
+    const [cx, cy] = shape.center
+    circle.setAttribute('cx', cx)
+    circle.setAttribute('cy', cy)
+    circle.setAttribute('r', shape.radius)
+    const stylestr = Object.keys(styles).reduce((acc, item) => {
+      return `${acc}${item}:${styles[item]};`
+    }, '')
+    circle.setAttribute('style', stylestr)
+    this.setElement(circle)
+  }
+
+  setAreas () {
+    const [cx, cy] = this.shape.center
+    const radius = this.shape.radius
+    this.areas = [cx - radius, cy - radius, cx + radius, cy + radius]
+  }
+}
