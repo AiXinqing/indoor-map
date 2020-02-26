92d1d8501951faff31fe8009b16f05f2fd29310e
moonRider <solar.pom@gmail.com>
2020-02-26 16:46:40 +0800

    update publish path

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     filename: '[name]-[hash].js',
     path: path.resolve(process.cwd(), './dist'),
-    publicPath: '/www/dist',
+    publicPath: '/dist',
   },
   module: {
     rules: [
